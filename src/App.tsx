204a3bc11cf27f4c03791f6032967ac8ef1dacae
nightfall <nightfall@pop-os.localdomain>
2024-06-15 16:48:54 +0200

    Update routing for static views

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Dashboard from "./pages/Dashboard";
 import Layout from "./layouts/main";
 import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
+import Leaderboard from "./pages/Leaderboard";
+import BattleArena from "./pages/BattleArena";
+import Market from "./pages/Market";
 
 function App() {
     return (
@@ -10,9 +13,9 @@ function App() {
             <Layout>
                 <Routes>
                     <Route path="/" element={<Dashboard />} />
-                    <Route path="/leaderboard" element={<Dashboard />} />
-                    <Route path="/arena" element={<></>} />
-                    <Route path="/market" element={<></>} />
+                    <Route path="/leaderboard" element={<Leaderboard />} />
+                    <Route path="/arena" element={<BattleArena />} />
+                    <Route path="/market" element={<Market />} />
                 </Routes>
             </Layout>
         </BrowserRouter>
